5a79fece1eda3e467c70368ec6d6e23401c8c914
Karthick3290 <karthichettiar3290@gmail.com>
2025-03-05 07:06:01 +0530

    Added 2 more subnets

--- flask-app/terraform/main.tf
+++ flask-app/terraform/main.tf
@@ -6,8 +6,38 @@ resource "aws_vpc" "main" {
 }
 
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.main.id
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main.id
   availability_zone = "us-east-1a"
-  cidr_block = "10.1.0.0/24"
+  cidr_block        = "10.1.0.0/24"
+  tags = {
+    Name = "network-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "us-east-1b"
+  cidr_block        = "10.1.1.0/24"
+  tags = {
+    Name = "network-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "us-east-1a"
+  cidr_block        = "10.1.2.0/24"
+  tags = {
+    Name = "network-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "us-east-1b"
+  cidr_block        = "10.1.3.0/24"
+  tags = {
+    Name = "network-private-subnet-2"
+  }
 }
