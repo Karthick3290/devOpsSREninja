4e06030e980e199cf8ba2e6d3e76e1a3ba656874
Karthick3290 <karthichettiar3290@gmail.com>
2025-03-08 23:23:47 +0530

    Github action setup

---
+++ flask-app/terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket = "flask-app-ninja-tfstate"
+    dynamodb_table = "terraform-state-locking"
+    region  = "us-east-1"
+    key    = "terraform.tfstate"
+  }
+}
