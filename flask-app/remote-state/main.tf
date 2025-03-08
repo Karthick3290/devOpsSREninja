be9cf32939fe9ba3d78a50d0a220207d5b651ed4
Karthick3290 <karthichettiar3290@gmail.com>
2025-03-08 23:26:23 +0530

    Github action setup

---
+++ flask-app/remote-state/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "twbeach"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "flask-app-ninja-tfstate"
+    lifecycle {
+      prevent_destroy = true
+    }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+    bucket = aws_s3_bucket.terraform_state.id
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+resource "aws_dynamodb_table" "table_state_locking" {
+  name = "terraform-state-locking"
+  hash_key = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+  deletion_protection_enabled = true
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
