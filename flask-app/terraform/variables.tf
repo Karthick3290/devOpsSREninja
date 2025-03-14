fec131b159f133d923aeef172146ebc492404bb7
Karthick3290 <karthichettiar3290@gmail.com>
2025-03-14 13:22:59 +0530

    Karthick | VPC networking changes without modules

--- flask-app/terraform/variables.tf
+++ flask-app/terraform/variables.tf
@@ -1,3 +1,19 @@
-variable "private_subnets_count" {
-  default = true
+variable "public_subnet_cidr" {
+  type = list(string)
+  default = [ "10.2.200.0/24","10.2.190.0/24" ]
+}
+
+variable "private_subnet_cidr" {
+  type = list(string)
+  default = [ "10.2.25.0/24","10.2.35.0/24" ]
+}
+
+variable "availability_zones" {
+  type = list(string)
+  default = [ "us-east-1a","us-east-1b" ]
+}
+
+variable "vpc_cidr" {
+  type = string
+  default = "10.2.0.0/16"
 }
