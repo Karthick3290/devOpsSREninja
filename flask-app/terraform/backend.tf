48f5554369e70f7e1e95fd9f7489e5157920ad62
Karthick3290 <karthichettiar3290@gmail.com>
2025-04-12 07:08:45 +0530

    Removing deletion protection

--- flask-app/terraform/backend.tf
+++ flask-app/terraform/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     encrypt        = true
     bucket         = "flask-tfstate"
-    dynamodb_table = "terraform-state-locking"
     region         = "us-east-1"
     key            = "terraform.tfstate"
+    use_lockfile = true
   }
 }
