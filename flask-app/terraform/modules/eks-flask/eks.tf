117087c986ad9d3edb089f67ec51b49e6bcb1be4
Karthick3290 <karthichettiar3290@gmail.com>
2025-04-12 08:00:36 +0530

    eks updates and folder structure

--- flask-app/terraform/modules/eks-flask/eks.tf
+++ flask-app/terraform/modules/eks-flask/eks.tf
@@ -36,10 +36,9 @@ resource "aws_eks_cluster" "flask_eks_cluster" {
   role_arn = aws_iam_role.eks_cluster_role.arn
   version  = var.eks_version
   vpc_config {
-
     endpoint_private_access = true
     endpoint_public_access  = true
-    
+
     subnet_ids = concat(
       module.networking_flask.aws_subnet_private_ids,
       module.networking_flask.aws_subnet_public_ids
