82526c430561aaead1e71e36c24fc141a7b028d0
Karthick3290 <karthichettiar3290@gmail.com>
2025-04-13 16:10:40 +0530

    2

--- flask-app/terraform/variables.tf
+++ flask-app/terraform/variables.tf
@@ -1,21 +1,21 @@
 variable "public_subnet_cidr" {
   type    = list(string)
-  # default = ["10.2.200.0/24", "10.2.190.0/24"]
+  default = ["10.2.200.0/24", "10.2.190.0/24"]
 }
 
 variable "private_subnet_cidr" {
   type    = list(string)
-  # default = ["10.2.25.0/24", "10.2.35.0/24"]
+  default = ["10.2.25.0/24", "10.2.35.0/24"]
 }
 
 variable "availability_zones" {
   type    = list(string)
-  # default = ["us-east-1a", "us-east-1b"]
+  default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "vpc_cidr" {
   type    = string
-  # default = "10.2.0.0/16"
+  default = "10.2.0.0/16"
 }
 
 variable "ami_type" {
@@ -29,9 +29,11 @@ variable "capacity_type" {
 }
 variable "disk_size" {
   type = number
+  default = 20
 }
 variable "eks_version" {
   type = string
+  default = "1.31"
 }
 
 variable "subnet_private_ids" {
