75822bbcb42bec7877bba151007a284efe498132
Karthick <56195227+Karthick3290@users.noreply.github.com>
2025-04-19 22:44:22 +0530

    Update managed-addons.tf

--- flask-app/terraform/modules/eks-flask/managed-addons.tf
+++ flask-app/terraform/modules/eks-flask/managed-addons.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.flask_eks_cluster.name
   addon_name = "coredns"
-  addon_version = "v1.10.1-eksbuild.1"
+  addon_version = "v1.11.4-eksbuild.2"
   resolve_conflicts_on_update = "PRESERVE"
 }
