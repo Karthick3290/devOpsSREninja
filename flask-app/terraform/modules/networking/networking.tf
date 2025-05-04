ce4c189097f8dcd66c5dd74b2bb22ee0fbfce94c
Karthick3290 <karthichettiar3290@gmail.com>
2025-05-04 15:00:32 +0530

    ami value

--- flask-app/terraform/modules/networking/networking.tf
+++ flask-app/terraform/modules/networking/networking.tf
@@ -34,8 +34,24 @@ resource "aws_internet_gateway" "network-igw" {
   }
 }
 
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
 resource "aws_instance" "nat-instance" {
-  ami = "ami-01179af425b2ee025"
+  ami = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
   subnet_id = aws_route_table.network-route-public.id
   # vpc_security_group_ids = 
