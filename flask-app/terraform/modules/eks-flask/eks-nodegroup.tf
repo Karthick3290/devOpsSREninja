11553d2c486ff8f81fa7ec48e169465dac91cb36
Karthick3290 <karthichettiar3290@gmail.com>
2025-05-04 14:38:25 +0530

    Updated the folder structure

--- flask-app/terraform/modules/eks-flask/eks-nodegroup.tf
+++ flask-app/terraform/modules/eks-flask/eks-nodegroup.tf
@@ -1,7 +1,46 @@
 
-data "aws_iam_role" "nodes_iam_role" {
-  name = "flask-nodes-iam-role"
+# data "aws_iam_role" "nodes_iam_role" {
+#   name = "flask-nodes-iam-role"
+# }
+
+# Role used to access nodes
+
+resource "aws_iam_role" "nodes_iam_role" {
+  name               = "flask-nodes-iam-role"
+  assume_role_policy = <<POLICY
+  {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
+POLICY
+  tags = {
+    name = "flask-nodes-iam-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "flask_EKSWorkerNodePolicy" {
+  role       = aws_iam_role.nodes_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "flask_EKSCNIPolicy" {
+  role       = aws_iam_role.nodes_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "flask_EC2_ECRPolicy" {
+  role       = aws_iam_role.nodes_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 
 resource "aws_security_group" "eks_nodes_sg" {
   name = "allow_eks"
--- flask-app/terraform/modules/eks-flask/eks.tf
+++ flask-app/terraform/modules/eks-flask/eks.tf
@@ -5,9 +5,36 @@
 # networking -> vpc,subnet, security groups
 # cluster endpoint access -> private,public or both with cidr block
 
-data "aws_iam_role" "eks_iam_role" {
-  name = "flask-eks-iam-role"
+# # Role used for accessing clusters
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "flask-eks-iam-role"
+  assume_role_policy = <<POLICY
+  {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
+POLICY
+  tags = {
+    name = "flask-eks-iam-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "flask_eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# data "aws_iam_role" "eks_iam_role" {
+#   name = "flask-eks-iam-role"
+# }
 
 resource "aws_eks_cluster" "flask_eks_cluster" {
   name     = "flask-eks-cluster"
